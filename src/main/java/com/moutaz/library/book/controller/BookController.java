488c998fce207d3471846adf3d3e2ceb97ad050d
MoutazFJ <moutazbusiness@gmail.com>
2026-03-19 04:36:57 +0300

    feat: add BookController with REST endpoints

---
+++ src/main/java/com/moutaz/library/book/controller/BookController.java
@@ -0,0 +1,52 @@
+package com.moutaz.library.book.controller;
+
+import com.moutaz.library.book.domain.Book;
+import com.moutaz.library.book.dto.BookRequest;
+import com.moutaz.library.book.service.BookService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/books")
+public class BookController {
+
+    private final BookService bookService;
+
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Book>> getAllBooks() {
+        return ResponseEntity.ok(bookService.findAll());
+    }
+
+    @GetMapping("/{isbn}")
+    public ResponseEntity<Book> getByIsbn(@PathVariable Integer isbn) {
+        return ResponseEntity.ok(bookService.findByIsbn(isbn));
+    }
+
+    @GetMapping("/genre/{genre}")
+    public ResponseEntity<List<Book>> getByGenre(@PathVariable String genre) {
+        return ResponseEntity.ok(bookService.findByGenre(genre));
+    }
+
+    @GetMapping("/author/{author}/count")
+    public ResponseEntity<Long> countByAuthor(@PathVariable String author) {
+        return ResponseEntity.ok(bookService.countByAuthor(author));
+    }
+
+    @PostMapping
+    public ResponseEntity<Book> addBook(@RequestBody BookRequest request) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(bookService.addBook(request));
+    }
+
+    @DeleteMapping("/{isbn}")
+    public ResponseEntity<Void> deleteBook(@PathVariable Integer isbn) {
+        bookService.deleteByIsbn(isbn);
+        return ResponseEntity.noContent().build();
+    }
+}
