b3ce0dfd4052123d11b6429fa57ba011577af69e
MoutazFJ <moutazbusiness@gmail.com>
2026-03-21 23:21:31 +0300

    refactor: remove redundant getters and setters in BookRequest class using Lombok

--- src/main/java/com/moutaz/library/book/dto/BookRequest.java
+++ src/main/java/com/moutaz/library/book/dto/BookRequest.java
@@ -2,7 +2,13 @@ package com.moutaz.library.book.dto;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class BookRequest {
 
     @NotNull(message = "ISBN is required")
@@ -27,35 +33,4 @@ public class BookRequest {
     private Integer numOfPages;
     private Boolean hardcover;
     private Integer sizeMb;
-
-    // Getters and setters
-    public Integer getIsbn()                    { return isbn; }
-    public void setIsbn(Integer isbn)           { this.isbn = isbn; }
-
-    public String getAuthor()                   { return author; }
-    public void setAuthor(String author)        { this.author = author; }
-
-    public String getTitle()                    { return title; }
-    public void setTitle(String title)          { this.title = title; }
-
-    public String getGenre()                    { return genre; }
-    public void setGenre(String genre)          { this.genre = genre; }
-
-    public String getBookType()                 { return bookType; }
-    public void setBookType(String bookType)    { this.bookType = bookType; }
-
-    public Integer getDuration()                { return duration; }
-    public void setDuration(Integer duration)   { this.duration = duration; }
-
-    public String getNarrator()                 { return narrator; }
-    public void setNarrator(String narrator)    { this.narrator = narrator; }
-
-    public Integer getNumOfPages()              { return numOfPages; }
-    public void setNumOfPages(Integer n)        { this.numOfPages = n; }
-
-    public Boolean getHardcover()               { return hardcover; }
-    public void setHardcover(Boolean hardcover) { this.hardcover = hardcover; }
-
-    public Integer getSizeMb()                  { return sizeMb; }
-    public void setSizeMb(Integer sizeMb)       { this.sizeMb = sizeMb; }
 }
