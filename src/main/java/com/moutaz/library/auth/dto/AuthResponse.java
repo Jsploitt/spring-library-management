7b7cdc1228a0c7a8e658494c41dd21e49c554393
MoutazFJ <moutazbusiness@gmail.com>
2026-03-21 21:14:21 +0300

    feat: add DTO classes for authentication requests and responses

---
+++ src/main/java/com/moutaz/library/auth/dto/AuthResponse.java
@@ -0,0 +1,25 @@
+package com.moutaz.library.auth.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Set;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class AuthResponse {
+
+    private String token;
+
+    @Builder.Default
+    private String type = "Bearer";
+
+    private Long userId;
+    private String username;
+    private String email;
+    private Set<String> roles;
+}
